53d4f059b9dc6d537d04859cb10d7426eaabc113
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-03-17 22:40:57 +0100

    Refactor VideoGameController for EF Core integration
    Updated VideoGameController to use Entity Framework Core for data access, replacing the in-memory list of video games. Removed static list and refactored methods to utilize asynchronous database calls. Commented out methods for getting, adding, updating, and deleting games, indicating they are no longer in use. This change enhances scalability and maintainability by shifting to a database-driven approach.

--- VideoGameApi/Controllers/VideoGamesController.cs
+++ VideoGameApi/Controllers/VideoGamesController.cs
@@ -1,80 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Data;
 
 namespace VideoGameApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class VideoGameController : ControllerBase
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
     {
-        static private List<VideoGame> videoGames = new List<VideoGame>
-        {
-            new VideoGame
-            {
-                Id = 1,
-                Title = "Spider-Man 2",
-                Platform = "ps 5",
-                Developer = "Insomniac Games",
-                Publisher = "Sony Interactive Entertainment"
-            }
-        };
+        private readonly VideoGameDbContext _context = context ;
 
         [HttpGet]
-        public ActionResult<List<VideoGame>> GetVideoGames()
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames()
         {
-            return Ok(videoGames);
+            return Ok(await _context.VideoGames.ToListAsync());
         }
 
 
-        [HttpGet("{id}")]
-        public ActionResult<List<VideoGame>> GetVideoGameById(int id)
-        {
-            var game = videoGames.FirstOrDefault(x => x.Id == id);
-            if(game is null)
-            {
-                return NotFound();
-            }
+        //[HttpGet("{id}")]
+        //public ActionResult<List<VideoGame>> GetVideoGameById(int id)
+        //{
+        //    var game = videoGames.FirstOrDefault(x => x.Id == id);
+        //    if(game is null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(game);
-        }
+        //    return Ok(game);
+        //}
 
-        [HttpPost]
-        public ActionResult<VideoGame> AddNewGame(VideoGame newGame)
-        {
-            if (newGame is null)
-                return BadRequest();
-
-
-            newGame.Id = videoGames.Max(g => g.Id) + 1;
-            return CreatedAtAction(nameof(GetVideoGameById),  new { id= newGame.Id },newGame);
-        }
-
-        [HttpPut]
-        public IActionResult UpdateVideoGame(int id, VideoGame updatedGame)
-        {
-            var game = videoGames.FirstOrDefault(game => game.Id == id);
-
-            if(game is null)
-            {
-                return NotFound();
-            }
-
-            game.Title = updatedGame.Title;
-            game.Publisher = updatedGame.Publisher;
-            game.Developer = updatedGame.Developer;
-            game.Platform = updatedGame.Platform;
-
-            return NoContent();
-        }
+        //[HttpPost]
+        //public ActionResult<VideoGame> AddNewGame(VideoGame newGame)
+        //{
+        //    if (newGame is null)
+        //        return BadRequest();
 
 
-        [HttpDelete]
-        public IActionResult DeleteVideoGame(int id)
-        {
-            var game = videoGames.FirstOrDefault(game => game.Id == id);
-            if (game is null)
-                return  NotFound();
-            videoGames.Remove(game);
-            return NoContent();
-        }
+        //    newGame.Id = videoGames.Max(g => g.Id) + 1;
+        //    return CreatedAtAction(nameof(GetVideoGameById),  new { id= newGame.Id },newGame);
+        //}
+
+        //[HttpPut]
+        //public IActionResult UpdateVideoGame(int id, VideoGame updatedGame)
+        //{
+        //    var game = videoGames.FirstOrDefault(game => game.Id == id);
+
+        //    if(game is null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    game.Title = updatedGame.Title;
+        //    game.Publisher = updatedGame.Publisher;
+        //    game.Developer = updatedGame.Developer;
+        //    game.Platform = updatedGame.Platform;
+
+        //    return NoContent();
+        //}
+
+
+        //[HttpDelete]
+        //public IActionResult DeleteVideoGame(int id)
+        //{
+        //    var game = videoGames.FirstOrDefault(game => game.Id == id);
+        //    if (game is null)
+        //        return  NotFound();
+        //    videoGames.Remove(game);
+        //    return NoContent();
+        //}
     }
 }
